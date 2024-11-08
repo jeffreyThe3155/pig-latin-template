6dd31184a31e9b21ae560826c08143c43889b29b
jeffreyThe3155 <jjl28282@gmail.com>
2024-11-08 21:22:32 +0000

    modified:   src/PigLatinTranslator.java

--- src/PigLatinTranslator.java
+++ src/PigLatinTranslator.java
@@ -20,9 +20,22 @@ public class PigLatinTranslator
     // The input to this function could be any English string. 
     // A sentence, paragraph, or a single word. 
     // It should call translateWord at least once.
-    String result = translateWord(input);
+    //String[] words = input.split(" +");
+    StringBuilder result = new StringBuilder();
+    StringBuilder buffer = new StringBuilder();
+    for(char c : input.toCharArray()) {
+      if(c==' ') {
+        result.append(translateWord(buffer.toString()));
+        buffer.delete(0,buffer.length());
+        result.append(c);
+      } else buffer.append(c);
+    }
+    result.append(translateWord(buffer.toString()));
+    return result.toString();
+  }
 
-    return result;
+  private static boolean isConsonant(char input) {
+    return "aeiouyAEIOUY".indexOf(input) < 0;
   }
 
   private static String translateWord(String input)
@@ -32,7 +45,28 @@ public class PigLatinTranslator
     // Replace this code to correctly translate a single word.
     // Start here first!
     String result = input;
-    
+    /* if (input.substring(0, 1).equals("a") || input.substring(0, 1).equals("e") || input.substring(0, 1).equals("i") || input.substring(0, 1).equals("o") || input.substring(0, 1).equals("u") || input.substring(0, 1).equals("y")) {
+      result = input + "ay"
+    } else {
+      result = input
+      while (result.substring(0, 1).equals())
+    } */
+    if (input.length() == 0) {
+      return "";
+    }
+    if (isConsonant(input.charAt(0))) {
+      int index = 0;
+      StringBuilder consonants = new StringBuilder();
+
+      while (index < input.length() && isConsonant(input.charAt(index))) {
+        consonants.append(input.charAt(index++));
+      }
+      result = result.substring(index);
+      result += consonants;
+    }
+  
+    result += "ay";
+    System.out.println(result);
     return result;
   }
 
